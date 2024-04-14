1207d63708df4564f87e6ac52fa4e4deaef2fe31
Artem Lysiak <tiomalysiak@gmail.com>
2024-04-14 12:46:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,23 +3,23 @@ const modal = document.getElementById('myModal');
 const closeBtn = document.getElementById('closeBtn');
 const menuLinks = document.querySelectorAll('.mob-tab-link');
 
-button.addEventListener('click', function () {
-  modal.className = 'mob-tab is-open';
-});
+// button.addEventListener('click', function () {
+//   modal.className = 'mob-tab is-open';
+// });
 
-closeBtn.addEventListener('click', function () {
-  modal.className = 'mob-tab';
-});
+// closeBtn.addEventListener('click', function () {
+//   modal.className = 'mob-tab';
+// });
 
-menuLinks.forEach(link =>
-  link.addEventListener('click', function () {
-    modal.classList.toggle('is-open');
-  })
-);
+// menuLinks.forEach(link =>
+//   link.addEventListener('click', function () {
+//     modal.classList.toggle('is-open');
+//   })
+// );
 
-// function toggleModal() {
-//   modal.classList.toggle('is-open');
-// }
-// button.addEventListener('click', toggleModal);
-// closeBtn.addEventListener('click', toggleModal);
-// menuLinks.forEach(link => link.addEventListener('click', toggleModal));
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
+button.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
+menuLinks.forEach(link => link.addEventListener('click', toggleModal));
